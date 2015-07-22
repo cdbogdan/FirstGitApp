56d4164b9988c583b1ab715dea9954749f07abc9
Codrea Bogdan <cdbogdan@yahoo.com>
2015-07-22 16:17:57 +0300

    up

--- WebApplication1/Models/AccountViewModels.cs
+++ WebApplication1/Models/AccountViewModels.cs
@@ -12,10 +12,13 @@ namespace WebApplication1.Models
     public class ManageUserViewModel
     {
         [Required]
-        // ia sa vad
+        
+
+
+        // gigi
         [DataType(DataType.Password)]
         [Display(Name = "Current password")]
-        public string OldPassword { get; set; }
+        public string OldPassword { get; set; } 
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
